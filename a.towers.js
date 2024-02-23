1c4b45c97b6851f0a7259ccade83882870abf19c
Zac V <142675286+Oddzac@users.noreply.github.com>
2024-02-22 21:08:21 -0500

    .

--- a.towers.js
+++ a.towers.js
@@ -41,7 +41,7 @@ var towers = {
         // Proceed with repairs if the tower has more than 50% of its energy capacity
         if (tower.energy > (tower.energyCapacity * 0.5)) { 
             const criticalStructures = tower.room.find(FIND_STRUCTURES, {
-                filter: (structure) => structure.hits < structure.hitsMax * 0.1 && // Target at-risk structures
+                filter: (structure) => structure.hits < structure.hitsMax * 0.15 && // Target at-risk structures
                                         structure.structureType !== STRUCTURE_WALL &&
                                         structure.structureType !== STRUCTURE_RAMPART
             });
