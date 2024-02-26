7ae1c4301c761ec917078d53589dbc493e08fb25
Zac V <142675286+Oddzac@users.noreply.github.com>
2024-02-26 11:07:18 -0500

    .

--- a.memories.js
+++ a.memories.js
@@ -145,6 +145,7 @@ var memories = {
             const room = Game.rooms[roomName];
     
             // Initialize room memory object if it doesn't exist
+            if (!Memory.rooms[room.name].scoutingComplete) Memory.rooms[room.name].scoutingComplete = false;
             if (!Memory.rooms[roomName]) {
                 Memory.rooms[roomName] = {
                     phase: { Phase: 1, RCL: room.controller.level },
