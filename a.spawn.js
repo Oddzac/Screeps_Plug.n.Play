b85cc9ca768ead68852dc70cfb7324804979f65f
Zac V <142675286+Oddzac@users.noreply.github.com>
2024-02-26 13:02:53 -0500

    .

--- a.spawn.js
+++ a.spawn.js
@@ -120,7 +120,7 @@ var spawner = {
                 }
             }
             //console.log(`${totalEnergyRequired}`);
-            //console.log(`${JSON.stringify(desiredCounts)}`);
+            console.log(`${JSON.stringify(desiredCounts)}`);
             return desiredCounts;
         }
     },
