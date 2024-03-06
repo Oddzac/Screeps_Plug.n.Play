c537ce2a58e2bee125189210d45823bb320a96e6
Zac V <142675286+Oddzac@users.noreply.github.com>
2024-03-05 20:52:16 -0500

    .

--- role.hauler.js
+++ role.hauler.js
@@ -16,7 +16,8 @@ var roleHauler = {
         }
 
         this.signRoom (creep);
-        if (Game.time % 100 === 0 && creep.memory.task !== 'spawnHauler' || creep.memory.task !== 'linkHauler') {
+
+        if (Game.time % 100 === 0 && creep.memory.task !== 'spawnHauler' && creep.memory.task !== 'linkHauler') {
             this.assignCollectionTask(creep);
         }
         
