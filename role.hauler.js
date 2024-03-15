a0aef650f4654436aecbac93dfbc218d2128c177
Zac V <142675286+Oddzac@users.noreply.github.com>
2024-03-15 18:50:05 -0400

    Update role.hauler.js

--- role.hauler.js
+++ role.hauler.js
@@ -328,7 +328,7 @@ var roleHauler = {
             // Find the container closest to the room's controller
             if (containers.length > 0) {
                 let controller = creep.room.controller;
-                target = controller ? creep.pos.findinRange(containers, 3) : null;
+                target = controller ? creep.pos.findInRange(containers, 4) : null;
             }
         } else {
             // If there are spawns, extensions, or towers with free capacity, use the closest one
