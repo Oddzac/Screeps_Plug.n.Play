4a8fa69a8b02f31123d177abb5d867659104eca3
Zac V <142675286+Oddzac@users.noreply.github.com>
2024-03-17 20:42:53 -0400

    Update a.spawn.js

--- a.spawn.js
+++ a.spawn.js
@@ -28,7 +28,7 @@ calculateDesiredCounts: function(room) {
                 harvester: 1,
                 hauler: 2,
                 builder: 2,
-                upgrader: 0
+                upgrader: 1
             };
         }
 
