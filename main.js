0d30719efe940a525070642dc53309a2f3f06aaf
Zac V <142675286+Oddzac@users.noreply.github.com>
2024-04-11 20:10:54 -0400

    reimp profiler

--- main.js
+++ main.js
@@ -7,153 +7,158 @@ var linker = require('a.links');
 var terminals = require('a.terminal');
 var movement = require('a.movement');
 
+const profiler = require('screeps-profiler');
+
+// This line monkey patches the global prototypes.
+profiler.enable();
 module.exports.loop = function() {
+    profiler.wrap(function() {
 
-
-    //Globals
-    if (Game.time % 100 === 0) {
-        terminals.globalMarketManagement();
-    }
-
-    for (const roomName in Game.rooms) {
-        const room = Game.rooms[roomName];
-   
-        // Memory Cleanup
-        memories.immediateMemory();
-        terminals.cleanupOldOrders();
-
-        // Market Awareness Memory
-        if (Game.time % 200 === 0) {
-            //terminals.updateMarketPrices();
+        //Globals
+        if (Game.time % 100 === 0) {
+            terminals.globalMarketManagement();
         }
 
-        // Short-Term Memory
-        if (Game.time % 300 === 0) {
-            memories.shortTerm();
-            terminals.handleRoomTerminal(room);
+        for (const roomName in Game.rooms) {
+            const room = Game.rooms[roomName];
+    
+            // Memory Cleanup
+            memories.immediateMemory();
+            terminals.cleanupOldOrders();
+
+            // Market Awareness Memory
+            if (Game.time % 200 === 0) {
+                //terminals.updateMarketPrices();
+            }
+
+            // Short-Term Memory
+            if (Game.time % 300 === 0) {
+                memories.shortTerm();
+                terminals.handleRoomTerminal(room);
+                
+            }
+
+            // 1-hr Memory
+            if (Game.time % 1200 === 0) {
+                //terminals.generateMarketSummary();
+                
+            }
             
-        }
+            // Long-Term Memory
+            if (Game.time % 5000 === 0) {
+                memories.longTerm();
+            }
 
-        // 1-hr Memory
-        if (Game.time % 1200 === 0) {
-            //terminals.generateMarketSummary();
-            
-        }
-        
-        // Long-Term Memory
-        if (Game.time % 5000 === 0) {
-            memories.longTerm();
-        }
+            // Count the number of harvesters and haulers specifically in this room
+            const harvesters = _.filter(Game.creeps, (creep) => creep.memory.role === 'harvester' && creep.room.name === roomName);
+            const haulers = _.filter(Game.creeps, (creep) => creep.memory.role === 'hauler' && creep.room.name === roomName);
+            // Check the number of energy sources in the room
+            const energySources = room.find(FIND_SOURCES).length;
+            // Find available spawns in the room
+            const availableSpawn = room.find(FIND_MY_SPAWNS, {
+                filter: (spawn) => !spawn.spawning
+            })[0]; // Get the first available spawn
 
-        // Count the number of harvesters and haulers specifically in this room
-        const harvesters = _.filter(Game.creeps, (creep) => creep.memory.role === 'harvester' && creep.room.name === roomName);
-        const haulers = _.filter(Game.creeps, (creep) => creep.memory.role === 'hauler' && creep.room.name === roomName);
-        // Check the number of energy sources in the room
-        const energySources = room.find(FIND_SOURCES).length;
-        // Find available spawns in the room
-        const availableSpawn = room.find(FIND_MY_SPAWNS, {
-            filter: (spawn) => !spawn.spawning
-        })[0]; // Get the first available spawn
+            // Check if there is an available spawn in the room
+            if (availableSpawn) {
 
-        // Check if there is an available spawn in the room
-        if (availableSpawn) {
+                
 
-            
-
-            // Handle Spawning if there is only one energy source
-            if (energySources === 1) {
-                if (harvesters.length < 1 && haulers.length < 2) {
-                // Spawn a harvester if there are less than 1
-                availableSpawn.spawnCreep([MOVE, CARRY, WORK], `Harvester_${Game.time}`, {memory: {role: 'harvester', room: roomName}}); 
-                } else if (haulers.length < 2) {
-                    // Spawn a hauler if there are less than... additional logic needed since limit is 1
-                    availableSpawn.spawnCreep([CARRY, MOVE, MOVE], `Hauler_${Game.time}`, {memory: {role: 'hauler', room: roomName}});
-                } else {
-                    // If the minimum numbers of harvesters and haulers are met, manage other creep spawning as needed
-                    spawner.manageCreepSpawning(room);
-                }
-
-            } else {
-                if (harvesters.length < 1) {
+                // Handle Spawning if there is only one energy source
+                if (energySources === 1) {
+                    if (harvesters.length < 1 && haulers.length < 2) {
                     // Spawn a harvester if there are less than 1
                     availableSpawn.spawnCreep([MOVE, CARRY, WORK], `Harvester_${Game.time}`, {memory: {role: 'harvester', room: roomName}}); 
-                }  else if (haulers.length < 2) {
-                    // Spawn a hauler if there are less than 2
-                    availableSpawn.spawnCreep([CARRY, MOVE, MOVE], `Hauler_${Game.time}`, {memory: {role: 'hauler', room: roomName}});
+                    } else if (haulers.length < 2) {
+                        // Spawn a hauler if there are less than... additional logic needed since limit is 1
+                        availableSpawn.spawnCreep([CARRY, MOVE, MOVE], `Hauler_${Game.time}`, {memory: {role: 'hauler', room: roomName}});
+                    } else {
+                        // If the minimum numbers of harvesters and haulers are met, manage other creep spawning as needed
+                        spawner.manageCreepSpawning(room);
+                    }
+
                 } else {
-                    // If the minimum numbers of harvesters and haulers are met, manage other creep spawning as needed
-                    spawner.manageCreepSpawning(room);
+                    if (harvesters.length < 1) {
+                        // Spawn a harvester if there are less than 1
+                        availableSpawn.spawnCreep([MOVE, CARRY, WORK], `Harvester_${Game.time}`, {memory: {role: 'harvester', room: roomName}}); 
+                    }  else if (haulers.length < 2) {
+                        // Spawn a hauler if there are less than 2
+                        availableSpawn.spawnCreep([CARRY, MOVE, MOVE], `Hauler_${Game.time}`, {memory: {role: 'hauler', room: roomName}});
+                    } else {
+                        // If the minimum numbers of harvesters and haulers are met, manage other creep spawning as needed
+                        spawner.manageCreepSpawning(room);
+                    }
                 }
             }
-        }
-    
-        //Terminal Management
+        
+            //Terminal Management
 
-        if (!room.terminal || room.terminal.cooldown > 0) {
-            //console.log('Terminal is busy or cooling down.');
+            if (!room.terminal || room.terminal.cooldown > 0) {
+                //console.log('Terminal is busy or cooling down.');
 
-        } else if(room.terminal && room.controller && room.controller.my) {
-
+            } else if(room.terminal && room.controller && room.controller.my) {
 
 
-            //Manage Buys
-            if (Game.time % 10 === 0) {
-                //terminals.manageTerminal(room); 
-                //terminals.updatePL();  
+
+                //Manage Buys
+                if (Game.time % 10 === 0) {
+                    //terminals.manageTerminal(room); 
+                    //terminals.updatePL();  
+                    
+                }
+
+                //Check P&L & Manage Buys
+                if (Game.time % 50 === 0) {
+                    //terminals.adjustPrices(room);
+                    //terminals.purchaseUnderpricedResources(room);
+
+                }
+
+                //Manage Sell Prices
+                if (Game.time % 1200 === 0) {
+                    //Memory.marketData.PL.lastCredits = Game.market.credits;
+                    
+                }
+
                 
             }
 
-            //Check P&L & Manage Buys
-            if (Game.time % 50 === 0) {
-                //terminals.adjustPrices(room);
-                //terminals.purchaseUnderpricedResources(room);
+        
+            // Construction management, including checking for spawn construction
+            if (room.controller && room.controller.my) {
+                const spawns = room.find(FIND_MY_SPAWNS);
+                const spawnSites = room.find(FIND_CONSTRUCTION_SITES, {
+                    filter: { structureType: STRUCTURE_SPAWN }
+                });
 
+                if (spawns.length === 0 && spawnSites.length === 0) {
+                    construction.placeSpawn(room); // If no spawn in controlled room, place one
+                } else {
+                    construction.manageConstruction(room); // Handle other construction tasks
+                }
             }
 
-            //Manage Sell Prices
-            if (Game.time % 1200 === 0) {
-                //Memory.marketData.PL.lastCredits = Game.market.credits;
-                
-            }
-
-            
-        }
-
-    
-        // Construction management, including checking for spawn construction
-        if (room.controller && room.controller.my) {
-            const spawns = room.find(FIND_MY_SPAWNS);
-            const spawnSites = room.find(FIND_CONSTRUCTION_SITES, {
-                filter: { structureType: STRUCTURE_SPAWN }
+            // Towers
+            const myTowers = room.find(FIND_MY_STRUCTURES, {
+                filter: { structureType: STRUCTURE_TOWER }
             });
 
-            if (spawns.length === 0 && spawnSites.length === 0) {
-                construction.placeSpawn(room); // If no spawn in controlled room, place one
-            } else {
-                construction.manageConstruction(room); // Handle other construction tasks
+            myTowers.forEach(tower => {
+                towers.run(tower);
+            });
+            //Manage Links
+            const myLinksCount = room.find(FIND_MY_STRUCTURES, {
+                filter: { structureType: STRUCTURE_LINK }
+            });
+
+            if (myLinksCount.length > 1) {
+
+                //console.log(`calling linker for ${room}`);
+                linker.manageLinks(room);
             }
+            
         }
-
-        // Towers
-        const myTowers = room.find(FIND_MY_STRUCTURES, {
-            filter: { structureType: STRUCTURE_TOWER }
-        });
-
-        myTowers.forEach(tower => {
-            towers.run(tower);
-        });
-        //Manage Links
-        const myLinksCount = room.find(FIND_MY_STRUCTURES, {
-            filter: { structureType: STRUCTURE_LINK }
-        });
-
-        if (myLinksCount.length > 1) {
-
-            //console.log(`calling linker for ${room}`);
-            linker.manageLinks(room);
-        }
-        
-    }
+    });
 
 }
 
