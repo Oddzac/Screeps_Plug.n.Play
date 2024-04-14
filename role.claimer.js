94aa0ea4238b95155b1c787d447a77e9f440b8d9
Zac V <142675286+Oddzac@users.noreply.github.com>
2024-04-14 10:27:21 -0400

    .

--- role.claimer.js
+++ role.claimer.js
@@ -25,7 +25,7 @@ var roleClaimer = {
                 plainCost: 2,
                 swampCost: 10,
                 roomCallback: function(roomName) {
-                    let room = creep.room.name;
+                    let room = creep.room;
                     let costs = new PathFinder.CostMatrix;
 
                     room.find(FIND_STRUCTURES).forEach(function(struct) {
