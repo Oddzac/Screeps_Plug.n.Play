c693ff5784890604f6f93ab95b70c53c5fa9f39e
Zac V <142675286+Oddzac@users.noreply.github.com>
2024-04-14 18:43:43 -0400

    Update a.construction.js

--- a.construction.js
+++ a.construction.js
@@ -209,7 +209,7 @@ connectSpawnToPOIs: function(room) {
                 swampCost: 10,
                 roomCallback: function(roomName) {
                     let costs = new PathFinder.CostMatrix();
-                    Game.rooms[roomName].find(FIND_STRUCTURES).forEach(function(struct) {
+                    Game.rooms[room.name].find(FIND_STRUCTURES).forEach(function(struct) {
                         if (struct.structureType === STRUCTURE_ROAD) {
                             costs.set(struct.pos.x, struct.pos.y, 1);
                         } else if (struct.structureType !== STRUCTURE_CONTAINER &&
