6220fbb50fd4d1aee6b7ce39b8e22c427b941a62
Zac V <142675286+Oddzac@users.noreply.github.com>
2024-04-15 07:27:40 -0400

    Update role.harvester.js

--- role.harvester.js
+++ role.harvester.js
@@ -92,7 +92,7 @@ var roleHarvester = {
     
 
 passEnergy: function(creep) {
-    let targets = creep.pos.findInRange(FIND_STRUCTURES, 2, {
+    let targets = creep.pos.findInRange(FIND_STRUCTURES, 3, {
         filter: (structure) => {
             return [STRUCTURE_LINK, STRUCTURE_STORAGE, STRUCTURE_CONTAINER].includes(structure.structureType);
         }
