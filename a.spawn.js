1378a92c7231d867117c0ea50c2eccad39d4b0bc
Zac V <142675286+Oddzac@users.noreply.github.com>
2024-04-29 22:36:17 -0400

    Update a.spawn.js

--- a.spawn.js
+++ a.spawn.js
@@ -28,7 +28,7 @@ calculateDesiredCounts: function(room) {
         if (energySources === 1) {
             return {
                 harvester: 2,
-                hauler: 3,
+                hauler: 4,
                 builder: 1,
                 upgrader: 1
             };
