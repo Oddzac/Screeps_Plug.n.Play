05cc88e5c0d754a905ff763153eb718d73bf1602
Zac V <142675286+Oddzac@users.noreply.github.com>
2024-05-04 19:49:53 -0400

    Update a.terminal.js

--- a.terminal.js
+++ a.terminal.js
@@ -109,6 +109,9 @@ var marketManager = {
                             console.log(`[PurchaseResource] New Cost Basis for ${resource}: ${newCostBasis}`);
                         }
                         console.log(`[PurchaseResource] Purchased ${amountToBuy} ${resource} for ${orderToBuy.price} credits each. Total cost: ${orderToBuy.price * amountToBuy}`);
+
+    let newQuantity = masterTerminal.store[resource] || 0;
+    console.log(`[PurchaseResource] Current quantity of ${resource} in ${masterTerminal.room.name}: ${newQuantity}`);
                         
                     } else {
                         console.log(`[PurchaseResource] Purchase failed... ${result}`);
