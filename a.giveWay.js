311cedb15aa2aefc5f1196a18e9549bbde6079ec
Zac V <142675286+Oddzac@users.noreply.github.com>
2024-05-07 11:31:21 -0400

    .

--- a.giveWay.js
+++ a.giveWay.js
@@ -14,7 +14,6 @@ Standard version:
 
 Creep.prototype.giveWay = function()
 {
-    console.log("giveWay Called");
     if(this.fatigue)
         return;
     // We can't block others when we're actively moving
