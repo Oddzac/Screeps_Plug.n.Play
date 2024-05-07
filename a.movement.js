fc97aad00ae0e2ff6e19efa4935acfe00a4c726e
Zac V <142675286+Oddzac@users.noreply.github.com>
2024-05-07 12:46:13 -0400

    Update a.movement.js

--- a.movement.js
+++ a.movement.js
@@ -205,7 +205,7 @@ var movement = {
             const newPath = creep.pos.findPathTo(targetPos, {
                 range: effectiveRange,
                 //REMOVE COMMENT AFTER TRAFFIC
-                ignoreCreeps: true,
+                //ignoreCreeps: true,
                 });
 
 
