800ad97625786983dbb4d29dacc4aedea22be7a7
Zac V <142675286+Oddzac@users.noreply.github.com>
2024-05-12 15:08:22 -0400

    .

--- a.movement.js
+++ a.movement.js
@@ -101,6 +101,8 @@ findCachedPath: function(creep, target, defaultRange = 1) {
                 range: effectiveRange,
                 ignoreCreeps: false,
             });
+            const serializedPath = Room.serializePath(path);
+            Memory.pathCache[pathKey] = { path: serializedPath, time: Game.time };
         } else {
             // Execute movement
             moveResult = creep.moveByPath(path);
