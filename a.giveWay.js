f074bc7e7b892dce0f9a9ad73235e8d871a90fed
Zac V <142675286+Oddzac@users.noreply.github.com>
2024-05-12 13:58:06 -0400

    switch to streamline

--- a.giveWay.js
+++ a.giveWay.js
@@ -6,10 +6,10 @@ Usage:
     "Normal logic" includes anything where creep.move(), creep.moveTo() or creep.moveByPath() is called.
 
 
-Standard version:
-    The standard version tries to handle any issues that may arise from inefficient usage of creep.move() or creep.moveTo().
-    Essentially it tries to detect when the creep won't move despite either function returning OK, and allow it to give way in those cases as well.
-    This can happen if a creep attempts to move into a wall or structure.
+Streamlined version:
+    The streamlined version cuts out any handling of erroneous moves into walls or structures, instead trying to merely achieve the behavior in the simplest way possible.
+    Creeps may therefore still block others if they're trying to move into walls or structures.
+    Use the standard version instead if you cannot guarantee that this won't occur.
 */
 
 Creep.prototype.giveWay = function()
@@ -26,7 +26,7 @@ Creep.prototype.giveWay = function()
     if(!this.memory.blocking)
         return;
     // It's an old request that has timed out
-    if(Game.time + 1 > this.memory.blocking)
+    if(Game.time > this.memory.blocking)
     {
         delete this.memory.blocking;
         return;
@@ -64,12 +64,9 @@ Creep.prototype.move = function(target)
             if(blockingCreep && blockingCreep.my)
                 blockingCreep.memory.blocking = Game.time + 1; // Request that they move away either this tick, or the next
         }
-
-        if(canMoveInto(this.room, nextX, nextY))
-            this.hasMoved = true;
     }
-    else
-        this.hasMoved = true;
+
+    this.hasMoved = true;
 
     return move.call(this, target);
 }
@@ -78,83 +75,4 @@ function isOutOfBounds(x, y)
 {
     return x < 0 || x > 49
         || y < 0 || y > 49;
-}
-
-// Complex code below to handle erroneous moves
-
-function canMoveInto(room, x, y)
-{
-    const objects = room.lookAt(x, y);
-    for(let i = 0; i < objects.length; ++i)
-    {
-        switch(objects[i].type)
-        {
-            case LOOK_STRUCTURES:
-                if(OBSTACLE_OBJECT_TYPES.includes(objects[i].structure.structureType))
-                    return false;
-                break;
-            case LOOK_TERRAIN:
-                if(objects[i].terrain === "wall")
-                    return false;
-                break;
-        }
-    }
-    return true;
-}
-
-const moveTo = Creep.prototype.moveTo;
-Creep.prototype.moveTo = function(firstArg, secondArg, opts)
-{
-    const result = moveTo.call(this, firstArg, secondArg, opts);
-    switch(result)
-    {
-        case OK:
-            if(_.isObject(firstArg))
-                opts = _.clone(secondArg);
-            opts = opts || {};
-        
-            const [x, y, roomName] = fetchXYArguments(firstArg, secondArg)
-            opts.range = opts.range || 1;
-            if(roomName)
-            {
-                if(!this.pos.inRangeTo(new RoomPosition(x, y, roomName), opts.range))
-                    this.hasMoved = true;
-            }
-            else if(!this.pos.inRangeTo(x, y, opts.range))
-                this.hasMoved = true;
-            break;
-    }
-    return result;
-}
-
-// Taken from Screeps engine source code https://github.com/screeps/engine/blob/97c9d12385fed686655c13b09f5f2457dd83a2bf/src/utils.js#L34-L63
-function fetchXYArguments(firstArg, secondArg) {
-    var x,y, roomName;
-    if(_.isUndefined(secondArg) || !_.isNumber(secondArg)) {
-        if(!_.isObject(firstArg)) {
-            return [undefined,undefined,undefined];
-        }
-
-        if(firstArg instanceof RoomPosition) {
-            x = firstArg.x;
-            y = firstArg.y;
-            roomName = firstArg.roomName;
-        }
-        if(firstArg.pos && (firstArg.pos instanceof RoomPosition)) {
-            x = firstArg.pos.x;
-            y = firstArg.pos.y;
-            roomName = firstArg.pos.roomName;
-        }
-    }
-    else {
-        x = firstArg;
-        y = secondArg;
-    }
-    if(_.isNaN(x)) {
-        x = undefined;
-    }
-    if(_.isNaN(y)) {
-        y = undefined;
-    }
-    return [x,y,roomName];
 }
