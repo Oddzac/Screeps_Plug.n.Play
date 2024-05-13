12aadd6166930dad4a7a5b96ba55f08046e0fa2e
Zac V <142675286+Oddzac@users.noreply.github.com>
2024-05-12 21:50:47 -0400

    .

--- a.movement.js
+++ a.movement.js
@@ -96,7 +96,6 @@ findCachedPath: function(creep, target, defaultRange = 1) {
         if (isBlocking) {
             // Handle next position is blocked by working creep
             creep.say("👀");
-            console.log(`Work blockage in ${creep.room.name}`);
             // Generate new path
             path = creep.pos.findPathTo(targetPos, {
                 range: effectiveRange,
