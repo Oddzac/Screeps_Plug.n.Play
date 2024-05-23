8bf7147a1c5b37a5d8044eb013f142bac0b39fb1
Zac V <142675286+Oddzac@users.noreply.github.com>
2024-05-22 20:23:44 -0400

    .

--- a.memories.js
+++ a.memories.js
@@ -189,7 +189,7 @@ var memories = {
                     construct: {
                         extensionRadius: 0,
                         structureCount: {
-                            extensions: {built: 0, pending},
+                            extensions: {built: 0, pending: 0},
                             containers: {built: 0, pending: 0},
                             storage: {built: 0, pending: 0},
                             extractor: {built: 0, pending: 0},
